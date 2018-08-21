0a69d7bf0612d854f8bc79c5137e20b82f426f04
leolulu <348699103@qq.com>
2018-08-21 10:29:08 +0800

    hisup

--- 测试专用.py
+++ 测试专用.py
@@ -1,7 +1,4 @@
-import os
-
-file_d = r"E:\Python\PycharmProjects\hellowolrld\file_opreation_directory"
-file_d2 = 'ceshi1'
-file_n = '1.txt'
-
-print(os.path.join(file_d,file_d2,file_n))
+for i in range(0,3):
+    print(i)
+for i in range(3,6):
+    print(i)
